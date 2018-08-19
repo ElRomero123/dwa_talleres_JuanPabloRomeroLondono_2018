f5fdfd6f8a3a23ced7c81ad47b32707677c87e79
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-08-19 01:50:36 -0500

    Algunas funcionalidades para cargar las ilustraciones

--- taller1/js/load.js
+++ taller1/js/load.js
@@ -1,6 +1,26 @@
 window.onload = function()
 {
+    // Cargar credenciales del usuario
     document.getElementById('name').innerHTML = "Nombre: " + this.localStorage.getItem("name");
     document.getElementById('username').innerHTML = "Username: " + this.localStorage.getItem("username");
     document.getElementById('email').innerHTML = "Email: " + this.localStorage.getItem("email");
+
+    // Cargar ilustraciones del usuario
+    var requestURL = 'https://raw.githubusercontent.com/ElRomero123/dwa_talleres_JuanPabloRomeroLondono_2018/master/ilustrations.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() 
+    {
+        var superHeroes = request.response;
+        alert(superHeroes['members'].length);
+    }
+
+    document.getElementById('close').onclick = function() 
+    {  
+        document.location.href = "index.html";
+        localStorage.clear();
+    }
 }
