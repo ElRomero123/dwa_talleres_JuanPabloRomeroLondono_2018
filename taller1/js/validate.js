348bfaf183a1697bd7f5c2ea342bb55a813c2e69
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-08-18 19:55:41 -0500

    Validación de formatos en login

--- taller1/js/validate.js
+++ taller1/js/validate.js
@@ -6,24 +6,58 @@ window.onload = function()
         var id = document.getElementById('id').value;
         obtain(email,id);
     }
-}  
+}
 
 function obtain(email,id)
 {
-    fetch('https://jsonplaceholder.typicode.com/users/' + id)
-    .then(response => response.json())
-    .then(json => validate(json.email, email))
+    if(isNaN(id) || id.indexOf('.') != -1)
+    {
+        document.getElementById('warning').style.display = 'block';
+        document.getElementById('warning').innerHTML = "Formato de ID incorrecto!";
+
+        //Esconde los mensajes antiguos.
+        document.getElementById('danger').style.display = 'none';
+    }
+
+    else
+    {
+        if(email.indexOf('@') == -1)
+        {
+            document.getElementById('warning1').style.display = 'block';
+            document.getElementById('warning1').innerHTML = "Correo sin @. Formato incorrecto!";
+
+            //Esconde los mensajes antiguos.
+            document.getElementById('danger').style.display = 'none';
+        }
+
+        else
+        {
+            fetch('https://jsonplaceholder.typicode.com/users/' + id)
+            .then(response => response.json())
+            .then(json => validate(json.email, email))
+        }
+    }
 }
 
 function validate(dbEmail, email)
 {
     if(dbEmail == email)
     {
-        alert("Usuario correcto");
+        document.getElementById('success').style.display = 'block';
+        document.getElementById('success').innerHTML = "Usuario correcto!";
+
+        document.getElementById('danger').style.display = 'none';
+        document.getElementById('warning').style.display = 'none';
+        document.getElementById('warning1').style.display = 'none';
     }
 
     else
     {
-        alert("Usuario incorrecto");
+        document.getElementById('danger').style.display = 'block';
+        document.getElementById('danger').innerHTML = "Usuario incorrecto!";
+
+        //Esconde los mensajes antiguos.
+        document.getElementById('warning').style.display = 'none';
+        document.getElementById('warning1').style.display = 'none';
     }
 }
