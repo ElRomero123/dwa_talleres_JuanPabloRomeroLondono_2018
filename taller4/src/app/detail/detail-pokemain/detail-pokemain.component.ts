3a551fe592067ad42decc3aa49248022984b8537
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-10-30 17:58:58 -0500

    Cambios

--- taller4/src/app/detail/detail-pokemain/detail-pokemain.component.ts
+++ taller4/src/app/detail/detail-pokemain/detail-pokemain.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-detail-pokemain',
   templateUrl: './detail-pokemain.component.html',
-  styleUrls: ['./detail-pokemain.component.scss']
+  styleUrls: ['./detail-pokemain.component.scss'],
+  providers: [ PokemonService ]
 })
 export class DetailPokemainComponent implements OnInit 
 {
-
-  constructor(private _route: ActivatedRoute) 
+  public pokemons: JSON;
+  puntaje = 10;
+  id = this._route.snapshot.paramMap.get('id');
+  
+  constructor(private _route: ActivatedRoute, private _pokemonService: PokemonService) 
   { 
-    console.log(this._route.snapshot.paramMap.get('id'));
+    
   }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
+    this.pokemons = this._pokemonService.getPokemons();
   }
 
 }
