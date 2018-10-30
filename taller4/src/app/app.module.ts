0a25da097722cf96105b545bc170dcb656659e58
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-10-30 07:02:00 -0500

    Reutilización del header taller 3

--- taller4/src/app/app.module.ts
+++ taller4/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeHeaderComponent } from './home/home-header/home-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeHeaderComponent
   ],
   imports: [
     BrowserModule,
